d5632130aadba78d358c0c052f92ea3b67127743
D.M.D.U.Bandara <104001186+DilumBBandara@users.noreply.github.com>
2022-05-11 22:51:04 +0530

    Add files via upload

---
+++ basic_pkg.sh
@@ -0,0 +1,18 @@
+r="\e[31;1m" # red
+g="\e[32;1m" # green
+y="\e[33;1m" # yellow
+b="\e[34;1m" # blue
+p="\e[35;1m" #purple
+n="\e[36;1m" # i don't know this color name
+w="\e[0;1m" # white
+
+
+clear;figlet Basic_pkg |lolcat
+echo "Installing..........."
+sleep 4
+echo "....................."
+echo "....................."
+echo "....................."
+echo "....................."
+echo "....................."
+sleep 8;pkg update -y;pkg upgrade -y;pkg install python python2 ruby -y;pip install requests;pkg install figlet toilet -y;gem install lolcat;pkg install php openssh -y;pkg install wget nano fish -y;pkg install hydra -y;pkg install netcat -y;pkg install curl -y;pkg install perl -y;pkg install tor -y;pkg install nmap -y;pkg install w3m -y;apt install proot -y;pkg install moonbuggy -y;pkg install clang -y;apt install vim -y;apt install zip -y;apt install unzip unrar -y;pkg install proxychains-ng -y;clear;toilet Done |lolcat;cd $HOME;fish
