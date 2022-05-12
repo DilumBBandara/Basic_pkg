3799a3e12f80d7d3d1c053802fbf26634cc51da5
D.M.D.U.Bandara <104001186+DilumBBandara@users.noreply.github.com>
2022-05-12 11:14:40 +0530

    Add files via upload

---
+++ Update.sh
@@ -0,0 +1,4 @@
+figlet Updating |lolcat
+echo "............."
+echo "............."
+pkg update -y;pkg upgrade -y;mv update.sh $HOME;rm -rf Basic_pkg;git clone https://github.com/DilumBBandara/Basic_pkg.git;cd Basic_pkg;clear;toilet Done |lolcat
