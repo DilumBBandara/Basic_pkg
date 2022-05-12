6b56143943dbbe433a3e5e6abbbe3baeaa8345f0
D.M.D.U.Bandara <104001186+DilumBBandara@users.noreply.github.com>
2022-05-12 09:40:23 +0530

    Update basic.sh

--- basic.sh
+++ basic.sh
@@ -1,5 +1,1 @@
-sleep 8;pkg update -y;pkg upgrade -y;apt-get upgrade;pkg install python python2 ruby -y;pip install requests;pkg install figlet toilet -y;gem install lolcat;pkg install php openssh -y;pkg install wget nano fish -y;pkg install hydra -y;pkg install netcat -y;pkg install curl -y;pkg install perl -y;pkg install tor -y;pkg install nmap -y;pkg install w3m -y;apt install proot -y;pkg install moonbuggy -y;pip2 install requests;pkg install clang -y;apt install vim -y;apt install zip -y;apt install unzip unrar -y;pkg install proxychains-ng -y;apt install pip;apt install pip2;clear;toilet Done |lolcat;cd $HOME
-echo "Installation successful..."
-echo ".........................."
-echo ".........................."
-fish
+pkg update -y;pkg upgrade -y;apt-get upgrade;pkg install python python2 ruby -y;pip install requests;pkg install figlet toilet -y;gem install lolcat;pkg install php openssh -y;pkg install wget nano fish -y;pkg install hydra -y;pkg install netcat -y;pkg install curl -y;pkg install perl -y;pkg install tor -y;pkg install nmap -y;pkg install w3m -y;apt install proot -y;pkg install moonbuggy -y;pip2 install requests;pkg install clang -y;apt install vim -y;apt install zip -y;apt install unzip unrar -y;pkg install proxychains-ng -y;apt install pip;apt install pip2;clear;toilet Done |lolcat;cd $HOME
