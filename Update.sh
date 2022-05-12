6565a112f8b891df96fe83d0b58cb0c2939fc13a
D.M.D.U.Bandara <104001186+DilumBBandara@users.noreply.github.com>
2022-05-12 11:29:39 +0530

    Update Update.sh

--- Update.sh
+++ Update.sh
@@ -1,4 +1,4 @@
 figlet Updating |lolcat
 echo "............."
 echo "............."
-pkg update -y;pkg upgrade -y;mv update.sh $HOME;rm -rf Basic_pkg;git clone https://github.com/DilumBBandara/Basic_pkg.git;cd Basic_pkg;clear;toilet Done |lolcat
+pkg update -y;pkg upgrade -y;mv update.sh $HOME;cd $HOME;rm -rf Basic_pkg;git clone https://github.com/DilumBBandara/Basic_pkg.git;clear;toilet Done |lolcat;rm -rf Update.sh
