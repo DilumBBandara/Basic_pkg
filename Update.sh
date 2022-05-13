507e49e01674b1e7e819a10d5d4e59ca07adea01
D.M.D.U.Bandara <104001186+DilumBBandara@users.noreply.github.com>
2022-05-13 11:29:59 +0530

    Update Update.sh

--- Update.sh
+++ Update.sh
@@ -1,6 +1,6 @@
-figlet Updating |lolcat
-echo "........................."
-echo "........................."
-pkg update -y;pkg upgrade -y;cd $HOME;rm -rf Basic_pkg;git clone https://github.com/DilumBBandara/Basic_pkg.git;clear;toilet Done |lolcat
-echo "Update successful."
+figlet Updating
+echo "..................................."
+echo "..................................."
+pkg update -y;pkg upgrade -y;cd $HOME;rm -rf Basic_pkg;git clone https://github.com/DilumBBandara/Basic_pkg.git;clear;toilet Done
+echo "Update successful..."
 echo "Now you can run this tool again..."
